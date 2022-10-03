7a85904eec12d91483962d3795fa7935845178b2
rasyidev <habib.rasyid11@gmail.com>
2022-10-04 06:00:05 +0700

    Add validation tag

--- model/web/category_create_request.go
+++ model/web/category_create_request.go
@@ -2,5 +2,5 @@ package web
 
 // Struct untuk request pada API Create category sesuai API Spec
 type CategoryCreateRequest struct {
-	Name string
+	Name string `validate:"required, min=1, max=200"`
 }
