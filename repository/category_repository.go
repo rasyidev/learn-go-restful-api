f6aeec81ab14d5ef144aef4cf0b02e7a067ae052
rasyidev <habib.rasyid11@gmail.com>
2022-10-03 18:45:47 +0700

    modified:   repository/category_repository.go

--- repository/category_repository.go
+++ repository/category_repository.go
@@ -7,9 +7,9 @@ import (
 )
 
 type CategoryRepository interface {
-	Save(ctx context.Context, tx sql.Tx, category domain.Category) domain.Category
-	Update(ctx context.Context, tx sql.Tx, category domain.Category) domain.Category
-	Delete(ctx context.Context, tx sql.Tx, category domain.Category)
-	FindAll(ctx context.Context, tx sql.Tx) []domain.Category
-	FindById(ctx context.Context, tx sql.Tx, categoryId int) domain.Category
+	Save(ctx context.Context, tx *sql.Tx, category domain.Category) domain.Category
+	Update(ctx context.Context, tx *sql.Tx, category domain.Category) domain.Category
+	Delete(ctx context.Context, tx *sql.Tx, category domain.Category)
+	FindAll(ctx context.Context, tx *sql.Tx) []domain.Category
+	FindById(ctx context.Context, tx *sql.Tx, categoryId int) (domain.Category, error)
 }
