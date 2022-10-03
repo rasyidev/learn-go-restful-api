f596784f70c590804fe0ed1898a2e7bc0a90bdda
rasyidev <habib.rasyid11@gmail.com>
2022-10-04 05:45:06 +0700

    modified:   service/category_service.go

--- service/category_service.go
+++ service/category_service.go
@@ -11,6 +11,6 @@ type CategoryService interface {
 	Create(ctx context.Context, request web.CategoryCreateRequest) web.CategoryResponse
 	Update(ctx context.Context, request web.CategoryUpdateRequest) web.CategoryResponse
 	Delete(ctx context.Context, categoryId int)
-	FindAll(ctx context.Context)
-	FindById(ctx context.Context)
+	FindAll(ctx context.Context) []web.CategoryResponse
+	FindById(ctx context.Context, categoryId int) web.CategoryResponse
 }
