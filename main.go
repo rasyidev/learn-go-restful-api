cff94944b896ce5ce2d1176bad606973ac7f5d8b
rasyidev <habib.rasyid11@gmail.com>
2022-10-04 09:00:43 +0700

    Add routes

---
+++ main.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"learn-go-restful-api/app"
+	"learn-go-restful-api/controller"
+	"learn-go-restful-api/helper"
+	"learn-go-restful-api/repository"
+	"learn-go-restful-api/service"
+	"net/http"
+
+	_ "github.com/go-sql-driver/mysql"
+
+	"github.com/go-playground/validator/v10"
+	"github.com/julienschmidt/httprouter"
+)
+
+func main() {
+	db := app.NewDB()
+
+	validate := validator.New()
+	categoryRepository := repository.NewCategoryRepository()
+	categoryService := service.NewCategoryService(categoryRepository, db, validate)
+	categoryController := controller.NewCategoryController(categoryService)
+
+	router := httprouter.New()
+
+	router.GET("/api/categories", categoryController.FindAll)
+	router.GET("/api/categories/:categoryId", categoryController.FindById)
+	router.PUT("/api/categories/:categoryId", categoryController.Update)
+	router.POST("/api/categories", categoryController.Create)
+	router.POST("/api/categories/:categoryId", categoryController.Delete)
+
+	server := http.Server{
+		Addr:    "localhost:9090",
+		Handler: router,
+	}
+
+	err := server.ListenAndServe()
+	helper.PanicIfError(err)
+}
