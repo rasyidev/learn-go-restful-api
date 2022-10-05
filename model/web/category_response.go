0502d4f4c5f561d96ec8a44185e1f9c11cf7e86d
rasyidev <habib.rasyid11@gmail.com>
2022-10-05 11:35:23 +0700

    Add json tag

--- model/web/category_response.go
+++ model/web/category_response.go
@@ -2,6 +2,6 @@ package web
 
 // Struct untuk response pada API Find, FindAll, Update, dll category sesuai API Spec
 type CategoryResponse struct {
-	Id   int
-	Name string
+	Id   int    `json:"id"`
+	Name string `json:"name"`
 }
